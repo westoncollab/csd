3237566440992150653c10a6f3741c74ec7c145b
ws325433 <56073593+ws325433@users.noreply.github.com>
2022-11-23 14:53:14 +0000

    Added text to about box
    Text is from UCW website

--- App/teston-super-mare/src/pages/Landing/Landing.jsx
+++ App/teston-super-mare/src/pages/Landing/Landing.jsx
@@ -15,10 +15,9 @@ class Landing extends React.Component {
                 </Button>
             </div>
             <div class='about-text'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestiae tempora ex aperiam necessitatibus amet id enim explicabo facilis blanditiis optio numquam
-                 ut, voluptatibus sint officia, fuga quos facere dolore? Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing
-                  elit. Iusto, molestiae tempora ex aperiam necessitatibus amet id enim explicabo facilis blanditiis optio numquam
-                 ut, voluptatibus sint officia, fuga quos facere dolore?
+                <p>Situated in the sunny seaside town of Weston-super-Mare, we're proud to be the top higher education centre in North Somerset.</p>
+                <p>We provide over 45 degree level programmes and short courses that will enable you to reach the next level or help you kickstart your career. From employer-based higher apprenticeships to full-time degrees and Higher Technical Qualifications (HTQs), all of our programmes are in partnership with UWE Bristol, Bath Spa University and Hartpury College.</p>
+                <p>Our diverse range of courses attract learners from all walks of life. We're smaller in size, but bigger in opportunity. Our smaller classes mean learners get more attention and interaction from our industry connected lecturing staff as well as the incredible student support on offer from our HEART team.</p>
             </div>
         </div>
     }
