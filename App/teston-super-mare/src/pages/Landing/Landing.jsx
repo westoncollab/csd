1071ccbdf917a855e45e081591b541d41e40912b
ws325433 <56073593+ws325433@users.noreply.github.com>
2022-11-23 14:38:48 +0000

    landing page layout

--- App/teston-super-mare/src/pages/Landing/Landing.jsx
+++ App/teston-super-mare/src/pages/Landing/Landing.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Landing.css';
+import Button from '@mui/material/button';
+import { Link } from 'react-router-dom';
 
 class Landing extends React.Component {
     render() {
         return <div className='landing'>
             <div className='login-buttons'>
-                <button>Sign up</button>
-                <button>Log in</button>
+                <Button variant='outlined' size='large' color='secondary'>
+                    <Link to='/signin'>Sign up</Link>
+                </Button>
+                <Button variant='outlined' size='large' color='primary'>
+                    <Link to='/login'>Log in</Link>
+                </Button>
             </div>
-            <div>
+            <div class='about-text'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestiae tempora ex aperiam necessitatibus amet id enim explicabo facilis blanditiis optio numquam
                  ut, voluptatibus sint officia, fuga quos facere dolore? Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing
                   elit. Iusto, molestiae tempora ex aperiam necessitatibus amet id enim explicabo facilis blanditiis optio numquam
