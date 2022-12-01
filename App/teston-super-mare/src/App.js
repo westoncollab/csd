e758252d723fd77b6fadde6455bb3f6f10ca4b8e
ws325434 <WS325434@weston.ac.uk>
2022-12-01 17:21:46 +0000

    Draft not completed yet

--- App/teston-super-mare/src/App.js
+++ App/teston-super-mare/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Layout from './pages/Layout/Layout';
+import Login from './pages/Login/Login';
 
 function App() {
   return (<BrowserRouter>
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Landing />} />
+        <Route path="login" element={<Login />} /> 
       </Route>
     </Routes>
   </BrowserRouter>);
---
+++ App/teston-super-mare/src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+class Login extends React.Component {
+    render() {
+        return <div className="login">
+
+        </div>
+    }
+}
+
+export default Login
