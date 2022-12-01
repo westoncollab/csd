5ed11fd20ba24ebedc47b2dd87b54d85b5f083d2
Herian Egan <herian.egan@jisc.ac.uk>
2022-12-01 10:25:40 +0000

    switch to using mui Paper, reduce CSS

--- App/teston-super-mare/src/pages/Landing/Landing.jsx
+++ App/teston-super-mare/src/pages/Landing/Landing.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import './Landing.css';
-import Button from '@mui/material/Button';
+import { Button, Paper } from '@mui/material';
 
 class Landing extends React.Component {
     render() {
         return (<div className='landing'>
-            <div className='white-bg login-buttons'>
+            <Paper className='login-buttons'>
                 <Button variant='outlined' size='large' color='secondary' href='/signup'>
                     Sign up
                 </Button>
                 <Button variant='outlined' size='large' color='primary' href='/login'>
                     Log in
                 </Button>
-            </div>
-            <div className='white-bg'>
+            </Paper>
+            <Paper>
                 <p>Situated in the sunny seaside town of Weston-super-Mare, we're proud to be the top higher education centre in North Somerset.</p>
                 <p>We provide over 45 degree level programmes and short courses that will enable you to reach the next level or help you kickstart your career. From employer-based higher apprenticeships to full-time degrees and Higher Technical Qualifications (HTQs), all of our programmes are in partnership with UWE Bristol, Bath Spa University and Hartpury College.</p>
                 <p>Our diverse range of courses attract learners from all walks of life. We're smaller in size, but bigger in opportunity. Our smaller classes mean learners get more attention and interaction from our industry connected lecturing staff as well as the incredible student support on offer from our HEART team.</p>
-            </div>
+            </Paper>
         </div>);
     }
 }
--- App/teston-super-mare/src/pages/Registration/Registration.jsx
+++ App/teston-super-mare/src/pages/Registration/Registration.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import './Registration.css';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import Input from '@mui/material/Input';
-import FormHelperText from '@mui/material/FormHelperText';
-import Button from '@mui/material/Button';
+import { FormControl, InputLabel, Input, FormHelperText, Button, Paper } from '@mui/material';
 import RegistrationController from './Registration.controller';
-import { ThemeProvider } from '@mui/material';
 
 class Registration extends React.Component {
     constructor(props) {
@@ -46,58 +41,60 @@ class Registration extends React.Component {
     }
 
     render() {
-        return (<form className='white-bg registration column' onSubmit={this.handleSubmit}>
-            <div className='column form-inputs'>
+        return (<Paper className='registration'>
+            <form className='column' onSubmit={this.handleSubmit}>
+                <div className='column form-inputs'>
+                    <FormControl>
+                    <InputLabel htmlFor='firstName'>First name</InputLabel>
+                    <Input
+                        id='firstName' name='firstName'
+                        value={this.state.firstName}
+                        onChange={this.handleInputChange}
+                        type='input'
+                        required />
+                </FormControl>
                 <FormControl>
-                <InputLabel htmlFor='firstName'>First name</InputLabel>
-                <Input
-                    id='firstName' name='firstName'
-                    value={this.state.firstName}
-                    onChange={this.handleInputChange}
-                    type='input'
-                    required />
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor='lastName'>Last name</InputLabel>
-                <Input
-                    id='lastName' name='lastName'
-                    value={this.state.lastName}
-                    onChange={this.handleInputChange}
-                    type='input'
-                    required />
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor='email'>Email address</InputLabel>
-                <Input
-                    id='email' name='email'
-                    value={this.state.email}
-                    onChange={this.handleInputChange}
-                    aria-describedby='emailHelper'
-                    type='email'
-                    required />
-                <FormHelperText id='emailHelper'>Please use your university email.</FormHelperText>
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor='password'>Password</InputLabel>
-                <Input
-                    id='password' name='password'
-                    value={this.state.password}
-                    onChange={this.handleInputChange}
-                    aria-describedby='passwordHelper'
-                    type='password'
-                    required />
-                <FormHelperText id='passwordHelper'>Please set a strong password.</FormHelperText>
-            </FormControl>
-            </div>
-            <div className='form-buttons'>
-                <Button color='error' variant='outlined' href='/'>
-                    Cancel
-                </Button>
-                <Button color='success' variant='outlined' type='submit'>
-                    Submit
-                </Button>
-            </div>
-        </form>);
+                    <InputLabel htmlFor='lastName'>Last name</InputLabel>
+                    <Input
+                        id='lastName' name='lastName'
+                        value={this.state.lastName}
+                        onChange={this.handleInputChange}
+                        type='input'
+                        required />
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor='email'>Email address</InputLabel>
+                    <Input
+                        id='email' name='email'
+                        value={this.state.email}
+                        onChange={this.handleInputChange}
+                        aria-describedby='emailHelper'
+                        type='email'
+                        required />
+                    <FormHelperText id='emailHelper'>Please use your university email.</FormHelperText>
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor='password'>Password</InputLabel>
+                    <Input
+                        id='password' name='password'
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        aria-describedby='passwordHelper'
+                        type='password'
+                        required />
+                    <FormHelperText id='passwordHelper'>Please set a strong password.</FormHelperText>
+                </FormControl>
+                </div>
+                <div className='form-buttons'>
+                    <Button color='error' variant='outlined' href='/'>
+                        Cancel
+                    </Button>
+                    <Button color='success' variant='outlined' type='submit'>
+                        Submit
+                    </Button>
+                </div>
+            </form>
+        </Paper>);
     }
 }
 
