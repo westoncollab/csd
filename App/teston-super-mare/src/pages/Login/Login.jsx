55f78becffc38fb1c91e14e8556a09219f47f157
ws325434 <WS325434@weston.ac.uk>
2022-12-01 18:03:52 +0000

    UI Changes to add login screen

--- App/teston-super-mare/src/pages/Login/Login.jsx
+++ App/teston-super-mare/src/pages/Login/Login.jsx
@@ -6,6 +6,32 @@ import { Link } from 'react-router-dom';
 class Login extends React.Component {
     render() {
         return <div className="login">
+<div className="container">
+  <div className="screen">
+    <div className="screen__content">
+      <form className="login">
+        <div className="login__field">
+          <input
+            type="text"
+            className="login__input"
+            placeholder="Email"
+          />
+        </div>
+        <div className="login__field">
+          <input
+            type="password"
+            className="login__input"
+            placeholder="Password"
+          />
+        </div>
+        <button className="button login__submit">
+          <span className="button__text">Log In</span>
+          <i className="button__icon fas fa-chevron-right" />
+        </button>
+      </form>
+    </div>
+  </div>
+</div>
 
         </div>
     }
