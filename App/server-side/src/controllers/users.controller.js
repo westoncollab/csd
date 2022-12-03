746f4f3d3f70fb4a37f1ae890c4d3f766e9dc9cd
WS325813 <WS325813@weston.ac.uk>
2022-12-03 17:16:32 +0000

    Initial login route work: added a /login route and moved password hashing to the server

--- App/server-side/src/controllers/users.controller.js
+++ App/server-side/src/controllers/users.controller.js
@@ -1,16 +1,20 @@
+const bcrypt = require('bcrypt');
 
 class UsersController {
     constructor(db) {
         this.db = db;
     }
 
-    saveNewUser (req, res) {
+    saveNewUser(req, res) {
         const { firstName, lastName, email, password } = req.body;
 
+        // Salt automatically added bcrypt.
+        const hashedPassword = bcrypt.hashSync(password);
+
         this.db.query(`
-       INSERT INTO \`users\` (\`firstName\`, \`lastName\`, \`roleId\`, \`email\`, \`password\`)
-       VALUES (?, ?, ?, ?, ?);
-       `, [firstName, lastName, 3, email, password]).then(dbResponse => {
+        INSERT INTO \`users\` (\`firstName\`, \`lastName\`, \`roleId\`, \`email\`, \`password\`)
+        VALUES (?, ?, ?, ?, ?);
+        `, [firstName, lastName, 3, email, hashedPassword]).then(dbResponse => {
             if (dbResponse.affectedRows && dbResponse.affectedRows === 1) {
                 res.status(201).send('success');
             }
@@ -20,6 +24,31 @@ class UsersController {
             } else {
                 console.log(err);
                 res.send(err);
+            }
+        })
+    }
+
+    login(req, res) {
+        const { email, password } = req.body;
+
+        this.db.query(`
+        SELECT 
+            firstName, lastName, roleName, password 
+        FROM
+            users
+        INNER JOIN ON 
+            users.roleId = roles.roleId
+        WHERE
+            email = ?
+        `, [email]).then((rows) => {
+            if (rows.length === 0 || !bcrypt.compareSync(password, rows[0].hashedPassword)) {
+                res.status(400).send('Invalid username or password.');
+            } else {
+                // Update the last login time but don't wait for a response.
+                this.db.query('UPDATE users SET lastLoginTime = NOW() WHERE email = ?', [email]);
+
+                // TODO: Persistence: Sessions? Tokens? 
+                res.status(200).json({ email, firstName, lastName, roleName });
             }
         })
     }
