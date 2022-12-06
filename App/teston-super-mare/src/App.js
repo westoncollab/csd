b1289510764d35eb38e9aad37c4c05f10f22ad93
WS325813 <WS325813@weston.ac.uk>
2022-12-06 18:36:14 +0000

    Added a RequiresRole which displays an error message if the user role does not match the specified requiredRole

--- App/teston-super-mare/src/App.js
+++ App/teston-super-mare/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Stack, Typography, Button } from '@mui/material';
 import Landing from './pages/Landing/Landing';
 import Layout from './pages/Layout/Layout';
 import Registration from './pages/Registration/Registration';
@@ -9,48 +10,72 @@ import Test from "./pages/Test/Test";
 
 const users = new UsersService();
 
-function App() { 
+function App() {
     const [user, setUser] = React.useState(null);// eslint-disable-line
- 
+
     async function handleLogin(email, password) { // eslint-disable-line
         const user = await users.tryLogin(email, password);
         setUser(user);
     }
- 
+
     function handleLogout() { // eslint-disable-line
         setUser({ isAuthenticated: false });
     }
-    
-  return (<BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Landing />} />
-        <Route path='lecturer' element={<LecturerDashboard/>} />  
-        <Route path='signup' element={<Registration />} />
-        <Route path='test' element={<Test
-            testName='Programming 102'
-            testId={7}
-            subjects={['General Knowledge']}
-            createdByLecturer='Lenny Lecturer'
-            questions={[
-              { qid: 1, question: 'Which is the best flavour?', a: 'Orange', b: 'Rose', c: 'Marmite', answer: 'b'},
-              { qid: 2, question: 'How are you today?', a: 'True', b: 'False', answer: 'a'},
-              { qid: 3, question: 'Where is the Moon?', a: 'Up', b: 'Down', c: 'Left', d: 'Right', answer: 'd'},
-              { qid: 4, question: 'Who is the Prime Minister?', a: 'Them', b: 'You', c: 'Me', answer: 'c'},
-              { qid: 5, question: 'What question is this?', a: '6', b: '13', c: '5', answer: 'c'},
-              { qid: 8, question: 'What question was that?', a: '6', b: '13', c: '5', answer: 'c'},
-              // { qid: 7, question: 'How are you today?', a: 'True', b: 'False', answer: 'a'},
-              // { qid: 8, question: 'Where is the Moon?', a: 'Up', b: 'Down', c: 'Left', d: 'Right', answer: 'a'},
-              // { qid: 9, question: 'Who is the Prime Minister?', a: 'Them', b: 'You', c: 'Me', answer: 'b'},
-              // { qid: 10, question: 'What question is this?', a: '6', b: '13', c: '5', answer: 'c'},
-              // { qid: 11, question: 'What question was that?', a: '6', b: '13', c: '5', answer: 'c'},
-              // { qid: 12, question: 'Which is the best flavour?', a: 'Orange', b: 'Rose', c: 'Marmite', answer: 'b'}
-            ]}
-            userId={9}
-        />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>);
+
+    function RequiresRole({ requiredRoleName, children }) {
+        if (!user || user.roleName !== requiredRoleName) {
+            return (
+                <Stack spacing={2} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <Typography variant="h3">
+                        Error: Access Denied
+                    </Typography>
+                    <Button href="/" variant="outlined">
+                        Go home
+                    </Button>
+                </Stack>
+            )
+        }
+        return children
+    }
+
+    return (<BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route index element={<Landing />} />
+                <Route 
+                    path='lecturer' 
+                    element={
+                        <RequiresRole requiredRoleName="Lecturer">
+                            <LecturerDashboard />
+                        </RequiresRole>
+                    } 
+                />
+
+                <Route path='signup' element={<Registration />} />
+                <Route path='test' element={<Test
+                    testName='Programming 102'
+                    testId={7}
+                    subjects={['General Knowledge']}
+                    createdByLecturer='Lenny Lecturer'
+                    questions={[
+                        { qid: 1, question: 'Which is the best flavour?', a: 'Orange', b: 'Rose', c: 'Marmite', answer: 'b' },
+                        { qid: 2, question: 'How are you today?', a: 'True', b: 'False', answer: 'a' },
+                        { qid: 3, question: 'Where is the Moon?', a: 'Up', b: 'Down', c: 'Left', d: 'Right', answer: 'd' },
+                        { qid: 4, question: 'Who is the Prime Minister?', a: 'Them', b: 'You', c: 'Me', answer: 'c' },
+                        { qid: 5, question: 'What question is this?', a: '6', b: '13', c: '5', answer: 'c' },
+                        { qid: 8, question: 'What question was that?', a: '6', b: '13', c: '5', answer: 'c' },
+                        // { qid: 7, question: 'How are you today?', a: 'True', b: 'False', answer: 'a'},
+                        // { qid: 8, question: 'Where is the Moon?', a: 'Up', b: 'Down', c: 'Left', d: 'Right', answer: 'a'},
+                        // { qid: 9, question: 'Who is the Prime Minister?', a: 'Them', b: 'You', c: 'Me', answer: 'b'},
+                        // { qid: 10, question: 'What question is this?', a: '6', b: '13', c: '5', answer: 'c'},
+                        // { qid: 11, question: 'What question was that?', a: '6', b: '13', c: '5', answer: 'c'},
+                        // { qid: 12, question: 'Which is the best flavour?', a: 'Orange', b: 'Rose', c: 'Marmite', answer: 'b'}
+                    ]}
+                    userId={9}
+                />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
