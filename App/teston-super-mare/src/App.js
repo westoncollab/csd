3a5df5bc2e312de269de9a0a8a7275e635eb5405
ws325434 <56073594+ws325434@users.noreply.github.com>
2022-12-07 09:30:18 +0000

    Update App.js

--- App/teston-super-mare/src/App.js
+++ App/teston-super-mare/src/App.js
@@ -1,7 +1,3 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing/Landing';
-import Layout from './pages/Layout/Layout';
-
 function App() {
 
 import React from 'react';
